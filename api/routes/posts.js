6e65e8d71c4f4a8cfd8179ed29889457d7af213c
Trina <92634994+trinayau@users.noreply.github.com>
2022-04-20 15:02:37 +0100

    edited create

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require("body-parser");
 const postsController = require('../controllers/posts');
 
 router.get('/', postsController.index);
 router.get('/:id', postsController.show);
-router.post('/', bodyParser.json(), postsController.create)
+router.post('/', postsController.create)
 
 module.exports = router;
--- client/static/js/requests.js
+++ client/static/js/requests.js
@@ -1,3 +1,51 @@
-//when a client hits submit
-//post data to db
-//redirect client to show path e.g. /post/1
+async function getAll(){
+    try {
+        const response = await fetch(`http://localhost:3000/posts`);
+        const data = await response.json()
+        return data;
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
+async function getItem(category, id) {
+    try {
+        const response = await fetch(`http://localhost:3000/posts/${id}`);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
+async function makePost(e){
+    e.preventDefault();
+    try {
+        const options = {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+        }
+        
+        const response = await fetch('http://localhost:3000/posts', options);
+
+        const { id, err } = await response.json();
+        if(err) { 
+            throw Error(err) 
+        } else {
+            window.location.hash = `#posts/${id}`
+        }
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
+async function deleteBook(id){
+    try {
+        const options = { method: 'DELETE' }
+        await fetch(`http://localhost:3000/books/${id}`, options);
+        window.location.hash = `#books`
+    } catch (err) {
+        console.warn(err);
+    }
+}
