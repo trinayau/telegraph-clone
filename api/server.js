4dd6463b53d143bb45b2f2f6946204937af4b15e
Trina <92634994+trinayau@users.noreply.github.com>
2022-04-20 12:08:13 +0100

    Added package.json etc

--- api/server.js
+++ api/server.js
@@ -6,3 +6,8 @@ server.use(cors());
 server.use(express.json());
 
 const postRoutes = require('./routes/posts')
+const { rmSync } = require('fs')
+server.use('/posts', postRoutes)
+server.get('/', (req, res) => res.send("Welcome to Trina and Alfie's telegra.ph's clone"))
+
+module.exports = server
