ed26ed99c73290c5d5395cbbf96bc5aa6bcc7d06
Trina <92634994+trinayau@users.noreply.github.com>
2022-04-21 11:49:00 +0100

    now shows new post

--- client/static/js/modal.js
+++ client/static/js/modal.js
@@ -1,19 +1,77 @@
 const form = document.querySelector("form");
+const listOnPage = document.querySelector('.container');
+const API_URL = "http://localhost:3000/posts";
 
-form.addEventListener('click', createPost)
+function listNewPost(postId){
+    fetch(`http://localhost:3000/posts/${postId}`)
+        .then(response => response.json())
+        .then(post => {
+                const div = document.createElement('div');
+                div.classList.add("postContainer")
+                const header = document.createElement('h3');
+                header.textContent = post.title;
+                const contents = document.createElement('p');
+                contents.textContent = post.author;
+                const date = document.createElement('p');
+                date.textContent = post.content;
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(date);
+                const postDiv = document.getElementById("newPostDiv")
+                postDiv.appendChild(div);
+                form.style.display = "none";
+            })
+        .catch(error => {
+            console.log(error)
+        });
 
-async function createPost(e){
-   
-    try {
-        const options = {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({title: e.target.title.value, author: e.target.author.value, content: e.target.body.value})
-        }
-        const response = await fetch('http://localhost:3000/posts', options);
-        const post = await response.json();
-            window.location = `#posts/${post.id}`
-    } catch (err) {
-        console.error(err);
-    }
 }
+form.addEventListener("submit", async(event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const content = formData.get("body");
+    const post = {
+        title,
+        author,
+        content
+    };
+    const sentPost = await fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify({...post}),
+        headers: {
+            'content-type': 'application/json'
+        },
+    })
+    const newPost = await sentPost.json();
+    listNewPost(newPost.id)
+});
+
+
+// function listAllPosts() {
+//     fetch(API_URL)
+//         .then(response => response.json())
+//         .then(post => {
+//             post.forEach(post => {
+//                 const div = document.createElement('div');
+//                 div.classList.add("postContainer")
+//                 const header = document.createElement('h3');
+//                 header.textContent = post.title;
+//                 const contents = document.createElement('p');
+//                 contents.textContent = post.author;
+//                 const date = document.createElement('p');
+//                 date.textContent = post.content;
+//                 div.appendChild(header);
+//                 div.appendChild(contents);
+//                 div.appendChild(date);
+//                 form.appendChild(div);
+//             });
+//         });
+// }
+
+
+
+
+
+
